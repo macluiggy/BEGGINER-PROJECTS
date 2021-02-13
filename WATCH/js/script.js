2a72c872f056b352e4fddee5dc58421c0441843f
Luiggy <luiggyferrin@gmail.com>
2021-02-13 09:53:25 -0500

    javascript watch project finished

--- WATCH/js/script.js
+++ WATCH/js/script.js
@@ -1,29 +1,41 @@
-var container = document.getElementById('watch-container');
-var today = new Date();
-var hours = today.getHours();
-var mins = today.getMinutes();
-var secs = today.getSeconds()
-var greeting;
-var msg = document.createElement('div');
+(function () {
+    var container = document.getElementById('watch-container');
+    var greeting;
+    var msg = document.createElement('div');
+    var body = document.body;
+function updateTime() {
+    var date = new Date(),
+    hours = date.getHours(),
+    mins = date.getMinutes(),
+    secs = date.getSeconds()
 
+    if (mins < 10) {
+        mins = '0' + mins;
+    }
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    if (secs < 10) {
+        secs = '0' + secs;
+    }
 
-
-if (mins < 10) {
-    mins = '0' + mins;
+    if (hours > 18 ) {
+        greeting = 'Good evening';
+        body.className = 'evening';
+    } else if (hours > 12) {
+        greeting = 'Good afternoon';
+        body.className = 'afternoon';
+    } else {
+        greeting = 'Good morning';
+        body.className = 'morning';
+    }
+    
+    msg.innerHTML = hours + ':' + mins + ':' + secs + '<br>' + greeting
+    container.appendChild(msg)
 }
-if (hours < 10) {
-    hours = '0' + hours;
-}
+updateTime()
+var intervalo = setInterval(updateTime, 1000);
+
+}());
 
 
-
-if (hours > 18 ) {
-    greeting = 'Good evening';
-}else if (hours > 12) {
-    greeting = 'Good afternoon';
-} else {
-    greeting = 'Good morning';
-}
-
-msg.innerHTML = hours + ':' + mins + '<br>' + greeting
-container.appendChild(msg)
